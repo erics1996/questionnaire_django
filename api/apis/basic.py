a3e8fd8d65e5a0a48d1315d82d8b86c2992b3c4b
erics1996 <erics1996@yeah.net>
2020-08-11 23:38:56 +0800

    Customize the pager to realize the paging function of the questionnaire.

--- api/apis/basic.py
+++ api/apis/basic.py
@@ -3,6 +3,7 @@ from ..serializers import basic
 from app import models
 from rest_framework.response import Response
 from rest_framework import filters
+from rest_framework import pagination
 
 """从apiView可以知道rest_framework中的settings"""
 
@@ -29,6 +30,8 @@ class SurveysApi(ListAPIView):
     filter_backends = (filters.SearchFilter, CustomFilter)
     # 过滤的字段。配合filters.SearchFilter使用，如果想要支持更多的字段搜索，添加字段名称即可
     search_fields = ('grade__name',)
+    # 分页器
+    pagination_class = pagination.LimitOffsetPagination
     table_column = [
         {
             'prop': 'grade',
@@ -67,6 +70,11 @@ class SurveysApi(ListAPIView):
         # queryset = self.filter_queryset(self.get_queryset())  # self.get_queryset()返回一个queryset；filter_queryset：过滤queryset，返回的还是queryset；本质上就是调用self.queryset
         # queryset = self.get_queryset()  # 不要用 queryset = self.queryset，虽然本质上是一样的，不能直接调用
         queryset = self.filter_queryset(self.get_queryset())
+        # 帮助我们实现分页
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)  # serializer.data是分页后的数据
         serializer = self.get_serializer(queryset, many=True)  # 本质上也就是调用序列化类做实例化
         # return Response(serializer.data)
         return Response({
@@ -74,5 +82,22 @@ class SurveysApi(ListAPIView):
             'data': {
                 'table_column': self.table_column,
                 'table_data': serializer.data,
+            }
+        })
+
+    def get_paginated_response(self, data):
+        """
+        自定义分页返回的数据
+        :param data: serializer.data
+        :return:
+        """
+        return Response({
+            'code': 0,
+            'data': {
+                'table_column': self.table_column,
+                'table_data': {
+                    'data': data,
+                    'total': self.paginator.count
+                },
             }
         })
